3186ec6d0a53dba60c29f575c1a0712436c51670
Pgnop <penguingnop@gmail.com>
2025-10-17 11:57:11 +0800

    Added content

--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
 import './style.css'
 
-async function getExperienceYears(params) {
-  
-}
-const response = await fetch('./data.json');
-const data = await response.json();
-
-console.log(data);
-
+//experience years
+let experience = '1 year'
 
 document.querySelector('#app').innerHTML = /*html*/`
   <div class='header'>
@@ -27,7 +21,10 @@ document.querySelector('#app').innerHTML = /*html*/`
   <div class='content'>
     <section id='hero'>
       <h1>I like cats & code</h1>
-      <p>I'm Signaa James, a back-end engineer with </p>
+      <p>
+        Hi, I'm Signaa James, a back-end developer with less than ${experience} of experience. I first started teaching myself how to code for a few weeks, and later moved on to Codedex to learn the basics of JavaScript, Python, HTML, CSS, and Java. I'm currently continuing on Codecademy on the back-end path. In the future, I hope to build a team and start my own company.
+      </p>
+
     </section>
   </div>
 `
