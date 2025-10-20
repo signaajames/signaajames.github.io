fce9e4685fcc3c990ebab9578507298b0bd565b4
Pgnop <penguingnop@gmail.com>
2025-10-20 11:06:53 +0800

    Added style + fixed multi web page issue (w/ chatGPT)

--- blog/src/main.js
+++ blog/src/main.js
@@ -1,3 +1,5 @@
+import '../style.css'
+
 document.addEventListener('DOMContentLoaded', () => {
   const blog = document.querySelector('#blog')
   blog.innerHTML = `
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,38 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { resolve, join } from 'path'
+import fs from 'fs'
+
+const redirectToDir = () => ({
+  name: 'redirect-to-dir',
+  configureServer(server) {
+    server.middlewares.use((req, res, next) => {
+      const filePath = join(process.cwd(), req.url)
+      fs.stat(filePath, (err, stats) => {
+        // If a folder is requested, rewrite to index.html inside it
+        if (!err && stats.isDirectory()) {
+          if (!req.url.endsWith('/')) {
+            res.writeHead(301, { Location: req.url + '/' })
+            res.end()
+            return
+          }
+          req.url = req.url + 'index.html'
+        }
+        next()
+      })
+    })
+  }
+})
 
 export default defineConfig({
   base: '/',
+  plugins: [redirectToDir()],
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        blog: resolve(__dirname, 'blog/index.html')
+        blog: resolve(__dirname, 'blog/index.html'),
+        // blog1: resolve(__dirname, 'blog/blog1/index.html'),
+        // blog2: resolve(__dirname, 'blog/blog2/index.html'),
       }
     }
   }
