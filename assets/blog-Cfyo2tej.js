e0a9d67ff49bf3705455d4aa545737e8e98954c4
Pgnop <penguingnop@gmail.com>
2025-10-20 12:23:10 +0800

    Updates

---
+++ assets/blog-Cfyo2tej.js
@@ -0,0 +1,21 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";/* empty css              */const i=()=>{const e=document.querySelector("#blog");e&&(e.innerHTML=`
+    <div class='header'>
+      <h2 class='name'>SignaaJames</h2>
+      <p class='bio'>Back-end Engineer & Front-end Engineer</p>
+    </div>
+
+    <div class='navigation'>
+      <section class='navigations'>
+        <a class='hrefs' href="/" id='home'>Home</a>
+      </section>
+    </div>
+
+    <div class='content'>
+        <div class='BlogCard'>
+            <h2>Lorem ipsum</h2>
+            <p class='date'>October 15, 2025</p>
+            <p class='shortContent'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro rem eius esse repellat veniam excepturi vitae totam. Autem voluptatum earum debitis iure aperiam molestias, neque eveniet quam officia, quidem necessitatibus?</p>
+            <a class='readMore' href='./Lorem-ipsum'>Read more...</a>
+        </div>
+    </div>
+  `)};document.readyState==="loading"?document.addEventListener("DOMContentLoaded",i):i();
--- assets/blog-DdrWJ1H2.js
+++
@@ -1,4 +0,0 @@
-import"./modulepreload-polyfill-B5Qt9EMX.js";document.addEventListener("DOMContentLoaded",()=>{const e=document.querySelector("#blog");e.innerHTML=`
-    <h1>Hi</h1>
-    <p>Abc</p>
-  `});
