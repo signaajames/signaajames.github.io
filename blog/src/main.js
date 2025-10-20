aacad4a51cfb6b65c823947be0ae3a39f3078ffc
Pgnop <penguingnop@gmail.com>
2025-10-20 11:34:06 +0800

    test

--- blog/src/main.js
+++ blog/src/main.js
@@ -1,9 +1,47 @@
 import '../style.css'
 
-document.addEventListener('DOMContentLoaded', () => {
+let charHomeTextArray = []
+
+const initBlog = () => {
   const blog = document.querySelector('#blog')
-  blog.innerHTML = `
-    <h1>Hi</h1>
-    <p>Abc</p>
+  if (!blog) return
+
+  blog.innerHTML = /*html*/`
+    <div class='header'>
+      <h2 class='name'>SignaaJames</h2>
+      <p class='bio'>Back-end Engineer & Front-end Engineer</p>
+    </div>
+
+    <div class='navigation'>
+      <section class='navigations'>
+        <a class='hrefs' href="/" id='home'>Blog</a>
+        <a class='hrefs' href="#about">About</a>
+        <a class='hrefs' href="#skills">Skills</a>
+        <a class='hrefs' href="#projects">Projects</a>
+        <a class='hrefs' href="#contact">Contact</a>
+      </section>
+    </div>
+
+    <div class='content'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem officiis, debitis eius ut ea sequi quidem hic fugiat, delectus nisi ex iure reprehenderit velit rem ad et molestiae unde inventore!</p>
+    </div>
   `
-})
+
+  const homeLink = document.getElementById('home')
+  if (!homeLink) return
+
+  const homeText = homeLink.innerHTML
+  charHomeTextArray = homeText.split('')
+
+  changeText()
+}
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', initBlog)
+} else {
+  initBlog()
+}
+
+async function changeText() {
+  console.log(charHomeTextArray)
+}
