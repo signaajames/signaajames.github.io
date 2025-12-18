50f2d0f86f3231db01ee18084c6d7e02f3252e25
signaajames <penguingnop@gmail.com>
2025-12-18 19:16:24 +0800

    Updates

--- assets/main-78ZQdIms.js
+++ assets/main-78ZQdIms.js
@@ -1,6 +1,25 @@
 (function(){const a=document.createElement("link").relList;if(a&&a.supports&&a.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&o(s)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const i="/assets/catinerSS-D_MZzIHf.png",n="/assets/Screenshot%202025-11-18%20at%2012-36-50%20forum-BIvE-K1-.png",c=document.getElementById("app");c.innerHTML=`
   <div class='header'>
     <h2 class='name'>SignaaJames</h2>
+    <!-- Cloned socials from footer, shown in header -->
+    <section class='header-socials'>
+      <a href='mailto:SignaaJames@proton.me' class='header-social-link' aria-label='Email Signaa James'>
+        <svg id='mail' xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
+          <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+          <polyline points="22,6 12,13 2,6"></polyline>
+        </svg>
+      </a>
+      <a href='https://github.com/signaajames' class='header-social-link' target='_blank' rel='noreferrer' aria-label='GitHub profile'>
+        <svg id='github' xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-github'>
+          <path d='M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22'></path>
+        </svg>
+      </a>
+      <a href='https://x.com/SignaaJame' class='header-social-link' target='_blank' rel='noreferrer' aria-label='X/Twitter profile'>
+        <svg id='twitter' xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter">
+          <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+        </svg>
+      </a>
+    </section>
     <p class='bio'>Back-end Engineer & Front-end Engineer</p>
   </div>
 
@@ -59,8 +78,9 @@
           <div class='skillCard'>HTML</div>
           <div class='skillCard'>JavaScript</div>
           <div class='skillCard'>CSS</div>
+          <div class='skillCard'>React</div>
         </section>
-        <h3>Tools</h3>
+        <h3>Build & Tools</h3>
         <section class='skill-list'>
           <div class='skillCard'>Git</div>
           <div class='skillCard'>Vite</div>
@@ -140,13 +160,20 @@
       </nav>
       <div class='footer-social'>
         <a href='mailto:SignaaJames@proton.me' class='footer-link' aria-label='Email Signaa James'>
-          Email
+          <svg id='mail' xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z">
+            </path><polyline points="22,6 12,13 2,6"></polyline>
+          </svg>
         </a>
         <a href='https://github.com/signaajames' class='footer-link' target='_blank' rel='noreferrer' aria-label='GitHub profile'>
           <svg id='github' xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-github'>
             <path d='M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22'></path>
           </svg>
         </a>
+        <a href='https://x.com/SignaaJame' class='footer-link' target='_blank' rel='noreferrer' aria-label='X/Twitter profile'>
+          <svg id='twitter' xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter">
+            <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+          </svg>
+        </a>  
       </div>
     </section>
     <div class='footer-meta'>
