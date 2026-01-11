4aba2dac91dc9385f64995a2cd2f26e14a05137b
signaajames <penguingnop@gmail.com>
2026-01-11 17:54:32 +0800

    updated some of the about mes

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ app.innerHTML = /*html*/`
       <div class='hero-text'>
         <h1>I like cats & code.</h1>
         <h3 id='timezone'>UTC+8</h3>
-        <p>I like making web apps and stuff</p>
+        <p>System32 backup. Please do not delete.</p>
       </div>
 
       <section class='hero-buttons'>
@@ -46,10 +46,13 @@ app.innerHTML = /*html*/`
       <h1>About</h1>
       <section class='content-section'>
         <p>
-          I'm more of a backend developer since CSS is pretty hard for me as of.. writing this lol, though I still do it anyways.
-          <br>
-          <br>
-          What do I even say?
+        I build the parts of the internet you don’t see (cause I don't build parts of the internet.. not yet though)
+        <br>
+        <br>
+        I’m more of a backend developer because I prefer logic and data over fighting with flexbox and weird sizing
+        <br>
+        <br>
+        But I’m not a 'not my job' developer, I still write the CSS when the project needs it, even if I complain about it the whole time. I care more about the app actually working than making it look pretty, but I make sure it does both. Though the codebase ends up looking like cold sphagetti
         </p>
 
       </section>
@@ -154,7 +157,7 @@ app.innerHTML = /*html*/`
       </div>
     </section>
     <div class='footer-meta'>
-      <span>&copy; ${new Date().getFullYear()} Signaa James. All rights reserved.</span>
+      <span>&copy; ${new Date().getFullYear()} Signaa James. All rights reserved... or are they?</span>
     </div>
   </footer>
 `
